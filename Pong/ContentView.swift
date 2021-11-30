55d5969c7346fbc6089c9e0897a6fa656f31d454
Anand_Gogoi <anandgogoi2@gmail.com>
2021-11-30 01:22:24 -0800

    Initial Commit

---
+++ Pong/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pong
+//
+//  Created by Anand Gogoi on 11/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pong/PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongApp.swift
+//  Pong
+//
+//  Created by Anand Gogoi on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct PongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
